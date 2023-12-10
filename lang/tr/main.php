beb6498f32b0e18f4b9d270176744b8ef52ea4ca
Shamshieva <burulaiurbaeva7@gmail.com>
2023-12-10 11:14:37 +0600

    multilanguage

--- lang/tr/main.php
+++ lang/tr/main.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
-  'main' => 'Bashlik',
-    'home' => 'Bashlik'
+    'main' => 'Bashlik',
+    'home' => 'Anasayfa',
+    'about'=>'Biz hakkımızda',
+    'contact'=>'iletişim',
+    'search'=>'Aramak',
 ];
